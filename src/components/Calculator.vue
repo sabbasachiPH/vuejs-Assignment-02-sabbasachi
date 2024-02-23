65d26a66667e60d3ef01b6227d5acbebd3a55b9a
sabbasachiPH <rentustat02@gmail.comm>
2024-02-23 15:41:00 +0600

    calculator finished

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,22 +1,74 @@
 <script setup>
+import { ref, reactive } from "vue";
+
+const firstNumber = ref(0);
+const secondNumber = ref(0);
+const operation = ref("");
+const result = ref(0);
+
+function handleSubmit() {
+    if ('+' === operation.value) {
+        result.value = firstNumber.value + secondNumber.value
+    } else if ('-' === operation.value) {
+        result.value = firstNumber.value - secondNumber.value
+    } else if ('*' === operation.value) {
+        result.value = firstNumber.value * secondNumber.value
+    } else if ('/' === operation.value) {
+        result.value = firstNumber.value / secondNumber.value
+    } else {
+        result.value = "Something Wrong. Please Check"
+    }
+}
+
+function handleSwitchSubmit() {
+    switch (operation.value) {
+        case '+':
+            result.value = firstNumber.value + secondNumber.value;
+            break;
+        case '-':
+            result.value = firstNumber.value - secondNumber.value;
+            break;
+        case '*':
+            result.value = firstNumber.value * secondNumber.value;
+            break;
+        case '/':
+            result.value = firstNumber.value / secondNumber.value;
+            break;
+        default:
+            result.value = "Something Wrong. Please Check"
+    }
+
+}
 
 </script>
 <template>
     <div class="bg-red-100 flex-1">
         <h2 class="text-2xl p-5 text-center font-bold">Calculator</h2>
-        <input name="firstNum" class="m-5 p-5 bg-blue-200" type="number" placeholder="First Number" required="required"
-            id="">
-        <select name="operation" class="m-5 p-5 bg-blue-200" id="">
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <input type="number" name="secondNum" class="m-5 p-5 bg-blue-200" placeholder="Second Number" required="required"
-            id="">
-        <input type="button" value="Calculate" class="m-5 p-5 bg-green-300">
-        <p class="m-5 p-5 text-2xl font-black bg-gray-400 w-1/2">
-            Result:
+        <!-- 
+        <div class="text-center text-3xl">
+            {{ firstNumber }} {{ operation }} {{ secondNumber }} = {{ result }}
+        </div> -->
+        <div class="inputs flex">
+            <input v-model="firstNumber" class="m-3 p-5 bg-blue-200" type="number" placeholder="First Number"
+                required="required" id="">
+
+            <select v-model="operation" class="m-3 p-5 bg-blue-200" id="">
+                <option disabled value="">Please select one</option>
+                <option>+</option>
+                <option>-</option>
+                <option>*</option>
+                <option>/</option>
+            </select>
+            <input v-model="secondNumber" type="number" class="m-3 p-5 bg-blue-200" placeholder="Second Number"
+                required="required" id="">
+            <!-- <input @click="handleSubmit" type="button" value="Calculate" class="m-3 p-5 bg-green-300"> -->
+            <button @click="handleSubmit" type="submit" class="m-3 p-5 bg-green-300">Calculate</button>
+            <button @click="handleSwitchSubmit" type="submit" class="m-3 p-5 bg-green-300">Calculate Switch</button>
+        </div>
+
+
+        <p class="m-3 p-5 text-2xl font-black bg-gray-400 w-1/2">
+            Result: {{ result }}
         </p>
 
     </div>
